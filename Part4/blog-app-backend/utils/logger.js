c8b898aee063271f10135923cda5b58576fc7565
Luis Antonio Sanchez Islas <luis08islas@gmail.com>
2025-12-16 11:57:24 -0600

    excercise 4.2

---
+++ Part4/blog-app-backend/utils/logger.js
@@ -0,0 +1,9 @@
+const info = (...args) => {
+  console.log('[INFO]', ...args);
+}
+
+const error = (...args) => {
+  console.error('[ERROR]', ...args);
+}
+
+module.exports = { info, error }
---
+++ Part4/notes-backend/index.js
@@ -0,0 +1,7 @@
+const app = require('./app') // the actual Express application
+const config = require('./utils/config') // environment variables
+const logger = require('./utils/logger') // custom logger
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
